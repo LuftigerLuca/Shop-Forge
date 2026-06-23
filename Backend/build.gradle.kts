5b60e024b7a6ba3f1ef13633ef3cf6eaec81b10c
Luca Schumacher <l.schumacher@luftiger.eu>
2026-06-24 00:16:02 +0200

    tech: setup ktlint

--- Backend/build.gradle.kts
+++ Backend/build.gradle.kts
@@ -2,6 +2,32 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
 }
 
+val ktlintConfig by configurations.creating {
+    attributes {
+        attribute(Bundling.BUNDLING_ATTRIBUTE, getObjects().named(Bundling::class, Bundling.SHADOWED))
+    }
+}
+
+dependencies {
+    ktlintConfig(libs.ktlint)
+}
+
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
+}
+
+val ktlintCheck by tasks.registering(JavaExec::class) {
+    group = "verification"
+    description = "Check Kotlin code style"
+    classpath = ktlintConfig
+    mainClass.set("com.pinterest.ktlint.Main")
+    args("**/src/**/*.kt", "!**/build/**")
+}
+
+val ktlintFormat by tasks.registering(JavaExec::class) {
+    group = "formatting"
+    description = "Auto-format Kotlin code"
+    classpath = ktlintConfig
+    mainClass.set("com.pinterest.ktlint.Main")
+    args("-F", "**/src/**/*.kt", "!**/build/**")
 }
